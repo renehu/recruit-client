c54bfdf6533829a1ba06d376427d43998a8a64a9
renehu <19586178+renehu@users.noreply.github.com>
2022-04-03 19:10:46 +0800

    Client first push

---
+++ src/containers/login/login.jsx
@@ -0,0 +1,59 @@
+// login router component
+import React,{Component} from "react";
+import {
+    NavBar,
+    WingBlank,
+    List,
+    InputItem,
+    WhiteSpace,
+    Button
+    } from 'antd-mobile'
+
+import Logo from "../../components/logo/logo";
+
+const ListItem = List.Item;
+
+export default class Login extends Component{
+    state = {
+        username:'',
+        password:''
+    }
+
+    login = () => {
+        console.log(this.state)
+    }
+
+    handleChange = (item,value)=> {
+        this.setState({
+            [item]:value
+        })
+    }
+
+    gotoRegister = () => {
+        this.props.history.replace('/register')
+    }
+
+    render(){
+        return(
+            <div>
+                <WingBlank>
+                    <NavBar>RECRUIT</NavBar>
+                    <WhiteSpace/>
+                    <Logo/>
+                    <WhiteSpace/>
+                    <List>
+                        <WhiteSpace/>
+                        <InputItem onChange={val=>{this.handleChange('username',val)}} placeholder='Please enter your username'>Username</InputItem>
+                        <WhiteSpace/>
+                        <InputItem type="password" onChange={val=>{this.handleChange('password',val)}} placeholder='Please enter your password'>Password</InputItem>
+
+                        <WhiteSpace/>
+                        <Button type="primary" onClick={this.login}>Login</Button>
+                        <WhiteSpace/>
+                        <Button onClick={this.gotoRegister}>Go to register</Button>
+                    </List>
+                </WingBlank>
+            </div>
+        )
+    }
+}
---
+++ src/containers/main/main.jsx
@@ -0,0 +1,9 @@
+// main router component
+import React,{Component} from "react";
+export default class Main extends Component{
+    render(){
+        return(
+            <div>Main</div>
+        )
+    }
+}
