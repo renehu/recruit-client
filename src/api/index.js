60b7cef9c4220db77961ff3021fb3cbd4de4624e
renehu <rene.hu@outlook.com>
2024-08-20 17:13:23 +0800

    add api domain

--- src/api/index.js
+++ src/api/index.js
@@ -1,28 +1,30 @@
 // call API
 
-import ajax from './ajax';
+import ajax from "./ajax";
+
+const API_BASE_URL = "https://recruit.renehu.net/api";
 
 /* User */
-// user register
-export const reqRegister = (user) => ajax('/api/register', user, 'POST');
-
 // login
 export const reqLogin = ({ username, password }) =>
-  ajax('/api/login', { username, password }, 'POST');
+  ajax(`${API_BASE_URL}/login`, { username, password }, "POST");
+
+// user register
+export const reqRegister = (user) => ajax("/api/register", user, "POST");
 
 // update user data
-export const reqUpdateUser = (user) => ajax('/api/update', user, 'POST');
+export const reqUpdateUser = (user) => ajax("/api/update", user, "POST");
 
 // GET method for get user
-export const reqUser = () => ajax('/api/user');
+export const reqUser = () => ajax("/api/user");
 
 // GET method for get user list
-export const reqUserList = (type) => ajax('/api/userlist', { type });
+export const reqUserList = (type) => ajax("/api/userlist", { type });
 
 /* Chat */
 //export const reqSendMsg = (data) => ajax('/api/sendmsg', data, 'POST');
 
-export const reqMsgList = () => ajax('/api/msglist');
+export const reqMsgList = () => ajax("/api/msglist");
 
 // mark msg read
-export const reqMsgRead = (from) => ajax('/api/msgread', { from }, 'POST');
+export const reqMsgRead = (from) => ajax("/api/msgread", { from }, "POST");
