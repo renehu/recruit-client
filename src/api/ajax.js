b6f40cc95ac88865333401036bd3d368eb1120b4
renehu <rene.hu@outlook.com>
2024-08-21 13:55:55 +0800

    unblock socketio CORS

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,22 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
-export default function ajax(url, data = {}, type = 'GET') {
-  if (type === 'GET') {
+export default function ajax(url, data = {}, type = "GET") {
+  if (type === "GET") {
     // concat parma
     // data:{username:tom, password:123}
     // paramStr: username=tom&password=123
-    let paramStr = '';
+    let paramStr = "";
 
     Object.keys(data).forEach((key) => {
-      paramStr += key + '=' + data[key] + '&';
+      paramStr += key + "=" + data[key] + "&";
     });
 
     if (paramStr) {
       paramStr = paramStr.substring(0, paramStr.length - 1);
     }
 
-    return axios.get(url + '?' + paramStr);
+    return axios.get(url + "?" + paramStr, { withCredentials: true });
   } else {
-    return axios.post(url, data);
+    return axios.post(url, data, { withCredentials: true });
   }
 }
