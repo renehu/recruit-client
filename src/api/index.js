717f7d9c65b6a54fe929910fa43fe5a889a72e98
renehu <rene.hu@outlook.com>
2024-08-21 13:23:39 +0800

    update api urls

--- src/api/index.js
+++ src/api/index.js
@@ -10,21 +10,25 @@ export const reqLogin = ({ username, password }) =>
   ajax(`${API_BASE_URL}/login`, { username, password }, "POST");
 
 // user register
-export const reqRegister = (user) => ajax("/api/register", user, "POST");
+export const reqRegister = (user) =>
+  ajax(`${API_BASE_URL}/register`, user, "POST");
 
 // update user data
-export const reqUpdateUser = (user) => ajax("/api/update", user, "POST");
+export const reqUpdateUser = (user) =>
+  ajax(`${API_BASE_URL}/update`, user, "POST");
 
 // GET method for get user
 export const reqUser = () => ajax(`${API_BASE_URL}/user`);
 
 // GET method for get user list
-export const reqUserList = (type) => ajax("/api/userlist", { type });
+export const reqUserList = (type) => ajax(`${API_BASE_URL}/userlist`, { type });
 
 /* Chat */
-//export const reqSendMsg = (data) => ajax('/api/sendmsg', data, 'POST');
+export const reqSendMsg = (data) =>
+  ajax(`${API_BASE_URL}/sendmsg`, data, "POST");
 
-export const reqMsgList = () => ajax("/api/msglist");
+export const reqMsgList = () => ajax(`${API_BASE_URL}/msglist`);
 
 // mark msg read
-export const reqMsgRead = (from) => ajax("/api/msgread", { from }, "POST");
+export const reqMsgRead = (from) =>
+  ajax(`${API_BASE_URL}/msgread`, { from }, "POST");
